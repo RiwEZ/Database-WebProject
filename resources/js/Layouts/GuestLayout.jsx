50e6717da10cd03c3346e5f1d8ef1caecf2e7d7b
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2022-10-29 23:05:05 +0700

    fix: responsive login, register page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,19 +4,29 @@ import { Link } from "@inertiajs/inertia-react";
 
 export default function Guest({ children }) {
     return (
-        <div className="grid grid-cols-3 gap-0 w-full h-screen">
-            <div className="col-span-2 bg-[url('../img/boeing-767.png')] bg-no-repeat bg-[center_26rem] w-full h-full">
-                <div className="py-12 px-20">
+        <div className="w-full h-full flex flex-col sm:flex-row">
+            <div className="flex-1 bg-[url('../img/boeing-767.png')] bg-no-repeat sm:bg-[center_26rem]">
+                <div className="py-4 px-5">
                     <Link href="/">
-                    <h1 className="text-3xl font-bold">LEGEND MODEL SHOP</h1>
+                        <h1 className="text-3xl font-bold">
+                            LEGEND MODEL SHOP
+                        </h1>
                     </Link>
                 </div>
-                <div className="py-4 px-20">
-                        <h1 className="text-7xl font-bold leading-normal">DISCOVER<br/>FANTASTIC<br/>MODELS</h1>
+                <div className="py-10 px-5">
+                    <h1 className="text-5xl sm:text-7xl font-bold leading-normal">
+                        DISCOVER
+                        <br />
+                        FANTASTIC
+                        <br />
+                        MODELS
+                    </h1>
                 </div>
             </div>
-            <div className="grid col-span-1 bg-black w-full h-full justify-items-center">
-                <div className="bg-black p-20 w-full h-full">{children}</div>
+            <div className="flex-initial bg-black h-full justify-items-center">
+                <div className="bg-black p-10 sm:p-20 w-full h-full">
+                    {children}
+                </div>
             </div>
         </div>
     );
