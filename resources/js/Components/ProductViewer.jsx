853a5c3d602ec82e60308911fa798bc0bc2407f8
min23asdw <min23asdw@outlook.com>
2022-10-29 16:23:43 +0700

    Filter product without range_PRICE

--- resources/js/Components/ProductViewer.jsx
+++ resources/js/Components/ProductViewer.jsx
@@ -2,49 +2,90 @@ import React, { useEffect, useState } from "react";
 import { Link } from '@inertiajs/inertia-react';
 
 const type_filter = [
-    {name: "Classic Cars", selected: false},
+    {name: "Classic Car aas", selected: false},
     {name: "Motorcycles", selected: false},
     {name: "Planes", selected: false},
     {name: "Ships", selected: false},
     {name: "Trains", selected: false},
     {name: "Trucks and Buses", selected: false},
     {name: "Vintage Cars", selected: false},
-];
+];   // เอาไว้แค่ ประเภทรถ
+
+const price_filter = [{name: "By PRICE", selected: false},];  // ใส่่อย่างอื่นเพิ่มได้อีก ,scale ? year
 
 export default function ProductViewer({products, searchValue}) {
     const [minPrice, setMinPrice] = useState(0);
     const [maxPrice, setMaxPrice] = useState(99999);
-    const [filters, setFilters] = useState(type_filter);
+    const [type_filters, setType_filters] = useState(type_filter);
+    const [usePrice, setusePrice] = useState(price_filter);
 
-    function handleFilterChange(filter) {
-        setFilters(filters.map(f => {
+    function handleFilter(filter) {
+        setusePrice(usePrice.map(f => {
             if (f.name === filter.name) {
                return filter;
             }
             else {
                 return f;
             }
-        }));
+        }
+        ));
+    }
+    function handleFiltertype(type) {
+        setType_filters(type_filters.map(t => {
+            if (t.name === type.name) {
+               return type;
+            }
+            else {
+                return t;
+            }
+        }
+        ));
     }
 
+
     function checkFilter(product) {
-        let filter_selected = false;
-        for (let i = 0; i < filters.length; i++) {
-            if (filters[i].selected) {
-                filter_selected = true;
+        let type_selected = false;
+        for (let i = 0; i < type_filters.length; i++) {
+            if (type_filters[i].selected) {    
+                type_selected = true;
                 break;
             }
         }
-
+      
         let price = product.buyPrice;
-        if (!filter_selected && price >= minPrice && price <= maxPrice) {
+        if( !usePrice[0].selected){
+            if (  !type_selected   ) {
+                return true;
+            }
+            if (type_filters.find(f => f.name === product.productLine && f.selected) && price >= minPrice && price <= maxPrice) {
             return true;
+            }
         }
-        if (filters.find(f => f.name === product.productLine && f.selected)
-            && price >= minPrice && price <= maxPrice) {
-            return true;
+        if( usePrice[0].selected){
+            if (  !type_selected && price >= minPrice && price <= maxPrice    ) {
+                return true;
+            }
+            if (type_filters.find(f => f.name === product.productLine && f.selected) && price >= minPrice && price <= maxPrice) {
+                return true;
+                }
         }
+         
         return false;
+    }
+    
+
+    function range_PRICE() {
+        console.log(usePrice[0].selected)
+        if (usePrice[0].selected) {
+            return (<><div>        
+                <h4>PRICE RANGE</h4>                       
+                 <input type="number" min="0" value={minPrice} onChange={e => setMinPrice(parseFloat(e.target.value))}></input>
+                <input type="number" min="0" value={maxPrice} onChange={e => setMaxPrice(parseFloat(e.target.value))}></input>
+                </div>
+            </>)
+        }
+     
+        return "";
     }
 
     return (
@@ -53,36 +94,40 @@ export default function ProductViewer({products, searchValue}) {
             <div className="bg-white m-10 p-4">
                 <h3 className="text-2xl font-bold">Filter</h3>
                 <div>
-                    <h4>PRICE RANGE</h4>
-                    <input type="number" min="0" value={minPrice} onChange={e => setMinPrice(parseFloat(e.target.value))}></input>
-                    <input type="number" min="0" value={maxPrice} onChange={e => setMaxPrice(parseFloat(e.target.value))}></input>
-                </div>
-                <div>
-                    <h4>PRODUCT LINE</h4>
-                    <ul>
-                        {filters && filters.map((filter, i) =>
-                            <li key={i}>
+                {usePrice && usePrice.map(P =>
+                            <li>
                                 <input
                                     type="checkbox"
-                                    checked={filter.selected}
-                                    onChange={e => {
-                                        handleFilterChange({
-                                            ...filter,
-                                            selected: e.target.checked
-                                        })
-                                    }}
+                                    checked={P.selected}
+                                    onChange={e => {handleFilter({...P,selected: e.target.checked})}}
                                 ></input>
-                                <label className="ml-2">{filter.name.toUpperCase()}</label>
+                                <label className="ml-2">{P.name.toUpperCase()}</label>
+                        
+                                {range_PRICE()}
+                            </li>
+                        )}
+                </div>
+                <div>
+                    <h4>PRODUCT TYPE</h4>
+                    <ul>
+                        {type_filters && type_filters.map((typefilter ,  i) =>
+                            <li key={i} >
+                                <input
+                                    type="checkbox"
+                                    checked={typefilter.selected}
+                                    onChange={e => { handleFiltertype({ ...typefilter,selected: e.target.checked})}}
+                                ></input>
+                                <label className="ml-2">{typefilter.name.toUpperCase()}</label>
                             </li>
                         )}
                     </ul>
                 </div>
             </div>
             <div className="grid grid-cols-2 gap-10 my-10 pr-24">
-                {products &&
-                    products.filter(p => p.productName.toLowerCase().includes(searchValue.toLowerCase()))
+                {products &&  products.filter(p => p.productName.toLowerCase().includes(searchValue.toLowerCase()))
                     .filter(p => checkFilter(p))
                     .map(p =>
+
                     <div className="p-4 bg-white" key={p.productCode}>
                         <h4 className="text-2xl">{p.productName}</h4>
                         <p>{p.productDescription}</p>
