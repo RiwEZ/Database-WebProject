8d57ca4daf83402b077e328d78d93112954c053e
Pooh <purichseenuallae@gmail.com>
2022-11-06 05:19:11 +0700

    Redesign cart add sumprice function and some calculation function

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, Link } from '@inertiajs/inertia-react';
 
+function sumPrice(carts) {
+    let sum = 0;
+    {carts && Object.keys(carts).map((key, index) => {
+        sum += parseFloat(carts[key].buyPrice) * parseFloat(carts[key].quantity);
+    })}
+
+    return sum.toFixed(2);
+}
+
 export default function Cart({auth, cartBox, errors}) {
     const carts = cartBox;
     return (
@@ -12,9 +21,9 @@ export default function Cart({auth, cartBox, errors}) {
         >
             <Head title="Cart" />
 
-            <div class="bg-gray-100">
-                <div class="container mx-auto mt-10">
-                    <div class="flex shadow-md my-10">
+            <div class="bg-white">
+                <div class="max-w-7xl m-auto">
+                    <div class="flex border-4 border-black shadow-md my-10">
                         <div class="w-3/4 bg-white px-10 py-10">
                             <div class="flex justify-between border-b pb-8">
                                 <h1 class="font-semibold text-2xl">
@@ -24,7 +33,7 @@ export default function Cart({auth, cartBox, errors}) {
                                 <h2 class="font-semibold text-2xl">1 Items</h2>
                             </div>
                             <div class="flex mt-10 mb-5">
-                                <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">
+                                <h3 class="font-semibold text-gray-600 text-xs uppercase w-3/5">
                                     Product Details
                                 </h3>
                                 <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">
@@ -36,31 +45,29 @@ export default function Cart({auth, cartBox, errors}) {
                                 <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">
                                     Total
                                 </h3>
+                                <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/12">
+                                    
+                                </h3>
                             </div>
                             {carts && Object.keys(carts).map((key, index) =>
-                                                        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5" key={index}>
-                                                        <div class="flex w-2/5">
-                                                            <div class="w-20">
-                                                                <img
-                                                                    class="h-24"
-                                                                    src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                                                                    alt=""
-                                                                />
-                                                            </div>
+                                                        <div class="flex items-center  border-4 border-white -mx-8 px-6 py-5" key={index}>
+                                                        <div class="flex w-3/5">
                                                             <div class="flex flex-col justify-between ml-4 flex-grow">
-                                                                <span class="font-bold text-sm">
+                                                                <span class="font-bold text-xl">
                                                                     {carts[key].name}
                                                                 </span>
-                                                                <span class="text-red-500 text-md">
-                                                                    Scale {carts[key].scale}
+                                                                <div className='bg-white'>
+                                                                <div className='py-3'>
+                                                                    <div className='bg-black h-min w-min px-3 py-1'>
+                                                                    <span class="text-lg font-bold text-white">
+                                                                    {carts[key].scale}
                                                                 </span>
-                                                                <Link
-                                                                    href={`/remove-from-cart/${carts[key].id}`}
-                                                                    method="post"
-                                                                    class="font-semibold hover:text-red-500 text-gray-500 text-md"
-                                                                >
-                                                                    Remove
-                                                                </Link>
+                                                                </div>
+                                                                </div>
+                                                                
+                                                                
+                                                                </div>
+                                                                
                                                             </div>
                                                         </div>
                                                         <div class="flex justify-center w-1/5">
@@ -72,10 +79,11 @@ export default function Cart({auth, cartBox, errors}) {
                                                             </svg>
                         
                                                             <input
-                                                                class="mx-2 border text-center w-10"
+                                                                class="mx-2 border text-center w-20"
                                                                 type="text"
                                                                 value={carts[key].quantity}
                                                             />
+
                         
                                                             <svg
                                                                 class="fill-current text-gray-600 w-3"
@@ -85,26 +93,34 @@ export default function Cart({auth, cartBox, errors}) {
                                                             </svg>
                                                         </div>
                                                         <span class="text-center w-1/5 font-semibold text-sm">
-                                                        {carts[key].buyPrice}$
+                                                        $ {carts[key].buyPrice}
                                                         </span>
                                                         <span class="text-center w-1/5 font-semibold text-sm">
-                                                        {parseFloat(carts[key].buyPrice) * parseFloat(carts[key].quantity)}
+                                                        $ {parseFloat(carts[key].buyPrice) * parseFloat(carts[key].quantity)}
                                                         </span>
+
+                                                        <Link
+                                                                    href={`/remove-from-cart/${carts[key].id}`}
+                                                                    method="post"
+                                                                    class="text-center w-1/12 font-semibold hover:text-red-400 text-red-500 text-md"
+                                                                >
+                                                                    <span class="material-symbols-outlined align-middle">
+delete
+</span>
+                                                                    
+                                                                </Link>
                                                     </div>
                             )}
                             <a
                                 href="/"
-                                class="flex font-semibold text-indigo-600 text-sm mt-10"
+                                class="flex font-semibold text-l mt-10"
                             >
-                                <svg
-                                    class="fill-current mr-2 text-indigo-600 w-4"
-                                    viewBox="0 0 448 512"
-                                >
-                                    <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
-                                </svg>
                                 <Link
                                         href="/"
                                     >
+                                        <span class="material-symbols-outlined align-middle px-2">
+keyboard_backspace
+</span>
                                         Continue shopping
                                     </Link>
                             </a>
@@ -114,31 +130,26 @@ export default function Cart({auth, cartBox, errors}) {
                             <h1 class="font-semibold text-2xl border-b pb-8">
                                 Order Summary
                             </h1>
+                            <div className="flex flex-col justify-between">
+                            {carts && Object.keys(carts).map((key, index) => 
                             <div class="flex justify-between mt-10 mb-5">
-                                <span class="font-semibold text-sm uppercase">
-                                    Items 3
-                                </span>
-                                <span class="font-semibold text-sm">590$</span>
+                                
+                                                                <span class="font-semibold text-sm uppercase">
+                                                                {carts[key].name}
+                                                                </span>
+                                
+                                <span class="font-semibold text-sm">{parseFloat(carts[key].buyPrice) * parseFloat(carts[key].quantity)}</span>
                             </div>
-                            <div>
-                                <label class="font-medium inline-block mb-3 text-sm uppercase">
-                                    Shipping
-                                </label>
-                                <select class="block p-2 text-gray-600 w-full text-sm">
-                                    <option>Standard shipping - $10.00</option>
-                                </select>
-                            </div>
-                            <button class="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase my-10">
-                                Apply
-                            </button>
-                            <div class="border-t mt-8">
+                            )}
+                            <div class="border-t mt-8 justify-items-end">
                                 <div class="flex font-semibold justify-between py-6 text-sm uppercase">
                                     <span>Total cost</span>
-                                    <span>$600</span>
+                                    <span>$ {sumPrice(carts)}</span>
                                 </div>
-                                <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+                                <button class="border-4 border-black  bg-black font-semibold text-white hover:text-black hover:bg-white py-2 text-sm  uppercase w-full">
                                     Checkout
                                 </button>
+                            </div>
                             </div>
                         </div>
                     </div>
