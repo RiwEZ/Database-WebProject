b074186b7bdc6531003163f3a156771f88bd0ac7
Pooh <purichseenuallae@gmail.com>
2022-11-06 05:19:40 +0700

    Add navbar to welcome

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link, Head } from "@inertiajs/inertia-react";
 import ProductViewer from "@/Components/ProductViewer";
-import UserDropdown from "@/Components/UserDropdown";
+
+import Navbar from "@/Components/Navbar";
 
 export default function Welcome(props) {
     const [showNavbarMenu, setShowNavbarMenu] = useState(false);
@@ -32,44 +33,10 @@ export default function Welcome(props) {
                 ref={bannerRef}
             >
                 <div className="w-full">
-                    <div className="flex justify-between">
-                        <div className="p-4">
-                            <h1 className="text-3xl font-bold">
-                                LEGEND MODEL SHOP
-                            </h1>
-                        </div>
-
-                        <div className="p-4 mt-2 inline-block">
-                            {props.auth.user ? (
-                                <div className="flex flex-row">
-                                    <Link
-                                        href="cart"
-                                        className="text-md text-gray-700 font-semibold px-4"
-                                    >
-                                        CART
-                                    </Link>
-                                    <UserDropdown
-                                        username={props.auth.user.name}
-                                    />
-                                </div>
-                            ) : (
-                                <>
-                                    <Link
-                                        href={route("login")}
-                                        className="text-md text-gray-700 font-semibold"
-                                    >
-                                        LOGIN
-                                    </Link>
-                                    <Link
-                                        href={route("register")}
-                                        className="ml-4 text-md text-gray-700 font-semibold"
-                                    >
-                                        REGISTER
-                                    </Link>
-                                </>
-                            )}
-                        </div>
-                    </div>
+                    <Navbar
+                        auth={props.auth}
+                        showCart={true}
+                    /> 
                     <div className="h-full">
                         <h1 className="text-6xl font-bold leading-normal p-4 mb-16 text-black white-outline-text">
                             <span className="white-outline-text">HIGHLY DETAILED</span>
